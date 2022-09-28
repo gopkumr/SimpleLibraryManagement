541b1dfe4efc8153f66ebcfac6493c30a3be6b20
Gopakumar R <Gopakumar.R@live.com>
2022-09-28 20:17:03 +1000

    Added Identity API

---
+++ src/LibraryManagement/Identity/Identity.API/Config.cs
@@ -0,0 +1,56 @@
+﻿namespace Identity.API
+{
+    using IdentityServer4.Models;
+    using System.Collections.Generic;
+
+
+    public static class Config
+    {
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("Customer.Read", "Read Customer Data") { UserClaims = new[] { "user_level" } },
+                new ApiScope("Customer.Write", "Write Customer Data"){ UserClaims = new[] { "user_level" } },
+                new ApiScope("Customer.Delete", "Delete Customer Data"){ UserClaims = new[] { "user_level" } },
+
+                new ApiScope("Inventory.Read", "Read Inventory Data"){ UserClaims = new[] { "user_level" } },
+                new ApiScope("Inventory.Write", "Write Inventory Data"){ UserClaims = new[] { "user_level" } },
+                new ApiScope("Inventory.Delete", "Delete Inventory Data"){ UserClaims = new[] { "user_level" } },
+
+                new ApiScope("Lending.Read", "Read Lending Data"){ UserClaims = new[] { "user_level" } },
+                new ApiScope("Lending.Write", "Write Lending Data"){ UserClaims = new[] { "user_level" } },
+                new ApiScope("Lending.Delete", "Delete Lending Data"){ UserClaims = new[] { "user_level" } }
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "webapp",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("0D14BAAC-EF15-41C1-99A2-07DCCB1543BC".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "Customer.Read", "Customer.Write", "Customer.Delete", "Inventory.Read", "Inventory.Write", "Inventory.Delete", "Lending.Read", "Lending.Write", "Lending.Delete" }
+                }
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new List<ApiResource>
+            {
+                new ApiResource("Customer", "Customer API"){ Scopes=new[]{ "Customer.Read", "Customer.Write", "Customer.Delete" } },
+                new ApiResource("Inventory", "Inventory API"){Scopes=new[]{ "Inventory.Read", "Inventory.Write", "Inventory.Delete" } },
+                new ApiResource("Lending", "Lending API"){Scopes=new[]{ "Lending.Read", "Lending.Write", "Lending.Delete" } }
+            };
+    }
+}
+
+
--- src/LibraryManagement/Identity/Identity.API/Program.cs
+++ src/LibraryManagement/Identity/Identity.API/Program.cs
@@ -1,3 +1,6 @@
+using Identity.API;
+using System.Security.Cryptography;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddIdentityServer((serverOption) =>
+                    {
+                        serverOption.IssuerUri = "https://Identity.LibraryManagement";
+                    })
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryApiResources(Config.ApiResources)
+                .AddInMemoryClients(Config.Clients);
+
 
 var app = builder.Build();
 
@@ -16,10 +30,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseIdentityServer();
+
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.MapControllers();
+
 
 app.Run();
